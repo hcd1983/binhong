19b76cd380d9b867aafcc82eaa30a8ddc0296b7f
hcd1983 <hcd@mojopot.com>
2020-02-11 00:57:40 +0800

    index done

--- site.js
+++ site.js
@@ -17,8 +17,10 @@ var contact = {
 	barcode: "64915368",
 }
 
+var footer_bg = "images/footer-bg.jpg";
+
 var logo = "images/logo.svg";
-var logo_bw = "images/logo.jpg";
+var logo_bw = "images/logo_bw.svg";
 
 
 var site = {
@@ -30,10 +32,10 @@ var site = {
 };
 
 var nav = [
-			{text:"首頁",icon:``,path:"index.html"},
-			{text:`關於秉宏`,icon:`<i class="icon-angle-down"></i>`,path:"about.html"},
-			{text:`服務項目`,icon:`<i class="icon-angle-down"></i>`,path:"service.html"},
-			{text:`聯絡我們`,icon:`<i class="icon-angle-down"></i>`,path:"contact.html"},
+			{text:"首頁",icon:``,path:"index.html",current:"index"},
+			{text:`關於秉宏`,icon:`<i class="icon-angle-down"></i>`,path:"about.html",current:"about"},
+			{text:`服務項目`,icon:`<i class="icon-angle-down"></i>`,path:"service.html",current:"service"},
+			{text:`聯絡我們`,icon:`<i class="icon-angle-down"></i>`,path:"contact.html",current:"contact"},
 		];
 
 var whatWeDo = [
@@ -44,31 +46,31 @@ var whatWeDo = [
 
 	},
 	{
-		title: "合理交期與價錢",
+		title: "全檢服務",
 		des: "模組模具可快速更換產品，追求高效率的生產出貨，秉持合乎客戶成本的合理價錢，以「客戶最大效益」為首要目標。",
 		icon:"images/icons/money.svg",
 
 	},
 	{
-		title: "合理交期與價錢",
+		title: "專業技術/豐富經驗",
 		des: "模組模具可快速更換產品，追求高效率的生產出貨，秉持合乎客戶成本的合理價錢，以「客戶最大效益」為首要目標。",
 		icon:"images/icons/money.svg",
 
 	},
 	{
-		title: "合理交期與價錢",
+		title: "即時服務/資訊更新",
 		des: "模組模具可快速更換產品，追求高效率的生產出貨，秉持合乎客戶成本的合理價錢，以「客戶最大效益」為首要目標。",
 		icon:"images/icons/money.svg",
 
 	},
 	{
-		title: "合理交期與價錢",
+		title: "品質保證",
 		des: "模組模具可快速更換產品，追求高效率的生產出貨，秉持合乎客戶成本的合理價錢，以「客戶最大效益」為首要目標。",
 		icon:"images/icons/money.svg",
 
 	},
 	{
-		title: "合理交期與價錢",
+		title: "設備完善/快速打樣",
 		des: "模組模具可快速更換產品，追求高效率的生產出貨，秉持合乎客戶成本的合理價錢，以「客戶最大效益」為首要目標。",
 		icon:"images/icons/money.svg",
 
@@ -103,11 +105,18 @@ let header = new Vue({
 		image: site.logo,
     	image2x: site.logo,
     	path: site.base,
-		navigtaor:nav,
+		nav:nav,
 		contact:contact,
 		logoStyle:{
 			"width":"210px",
 		}
+	},
+	methods:{
+		currentPage: function(_current){
+
+			return _current == config._current;	
+
+		},
 	}
 })
 
@@ -123,7 +132,8 @@ let footer = new Vue({
 		nav:nav,
 		des:"穩定的產品，讓客戶信任我們的優質服務。合理的價格，讓我們確保客戶的最大利益，我們對自己工廠的實力感到自豪和信心。",
 		contact:contact,
-		des_2:"我們的上班週一至週五,8:30-17:30<br>歡迎聯絡，我們會儘快回覆您的訊息。"
+		des_2:"我們的上班週一至週五,8:30-17:30<br>歡迎聯絡，我們會儘快回覆您的訊息。",
+		bg_image:footer_bg,
 	}
 
 })
@@ -137,8 +147,8 @@ let sliderData = [
 			"background-color":"rgba(26,11,8,.5)",
 		},
 		position:"center",
-		html:`<h2 data-animate="fadeInUp">THE PUSHER <br>OF YOUR SUCCESS</h2>
-			  <h3 class="d-sm-block" data-animate="fadeInUp" data-delay="200">我們幫助您實現願景</h3>`,
+		html:`<h2 data-animate="fadeInUp" class="mb-2">THE PUSHER <br>OF YOUR SUCCESS</h2>
+			  <h3 data-animate="fadeInUp" data-delay="200">我們幫助您實現願景</h3>`,
 
 	},
 	{
@@ -200,7 +210,8 @@ let slider = new Vue({
 let _whatWeDo = new Vue({
 	el:"#whatwedo",
 	data:{
-		list:whatWeDo
+		list:whatWeDo,
+		addonClass:"shadowBottom",
 	}
 })
 
@@ -211,7 +222,7 @@ let about_us = new Vue({
 		title: "關於秉宏精密科技",
 		link:"#",
 		description:"秉宏精密位於台南市永康區，鄰近台南科學園區。擁有1988年至今的加工經驗，優秀的經營團隊，秉持著『人性化管理』，追求企業永續經營及成長。主要從事電子相關製造業，以射頻連接器SMA.SMB.MCX.MMCX.TNC...等系列為主軸，提供專業代工協助開發製造，同時也承接各類五金加工。",
-		image:"images/about.png"
+		image:"images/about.png",
 	}
 })
 
@@ -227,22 +238,22 @@ let service = new Vue({
 		title: "服務項目",
 		services:[
 			{
-				"icon":"images/icon.png",
+				"icon":"gear",
 				"title":"各類五金車铣加工",
 				"description":"我們是全台灣最大的汽車零件製造商之一，並且是公認的領導者……",
 				"link":"#",
 				isActive: false,
 			},
 			{
-				"icon":"images/icon.png",
-				"title":"各類五金車铣加工",
+				"icon":"gearman",
+				"title":"產品OEM代工/跨產業CNC代工",
 				"description":"我們是全台灣最大的汽車零件製造商之一，並且是公認的領導者……",
 				"link":"#",
 				isActive: false,
 			},
 			{
-				"icon":"images/icon.png",
-				"title":"各類五金車铣加工",
+				"icon":"note",
+				"title":"品質檢測設備",
 				"description":"我們是全台灣最大的汽車零件製造商之一，並且是公認的領導者……",
 				"link":"#",
 				isActive: false,
@@ -258,6 +269,9 @@ let service = new Vue({
 			}
 
 			this.services[index].isActive = true;
+		},
+		icon(type){
+			return icon(type);
 		}
 	}
 })
@@ -267,7 +281,7 @@ let trigger_box = new Vue({
 	el:"#trigger_box",
 	data:{
 		style:{
-			'background-image':'url(images/slider_02.jfif)',
+			'background-image':'url(images/trigger-1.png)',
 			'background-position':'center',
 			'background-size':'cover',
 			'min-height':'400px',
